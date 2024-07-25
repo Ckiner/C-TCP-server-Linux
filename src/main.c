d1f6897aad88eee64edcc6ca16781fa32890ce26
Christian Kiner <christiankiner21@icloud.com>
2024-07-25 14:21:59 -0500

    Refactored main

--- src/main.c
+++ src/main.c
@@ -7,17 +7,27 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-struct sockaddr_in createSockAddr(char * address, unsigned short port, short family) {
-    struct sockaddr_in data;
+void acceptConnections(int sockfd) {
+    struct sockaddr_in connaddr;
+    socklen_t connsize = sizeof(struct sockaddr_in);
+    int connfd = accept(sockfd, (struct sockaddr *)&connaddr, &connsize);
 
-    data.sin_family = family;  // e.g. AF_INET
-    data.sin_port = htons(port); // Transforms port to unsigned short in network byte order
-    inet_aton(address, &data.sin_addr);  // Transform ascii ip to an unsigned long in network byte order
+    if (connfd == -1) {
+        printf("Error connecting to socket.\n");
+        return 1;
+    }
 
-    return data;
+
+    char * message = "Hello world!";
+    send(connfd, message, strlen(message), 0);
+    close(connfd);
 }
 
 int main(int argc, char * argv[]) {
+    if (argc < 4) {
+        printf("Please specify all arguments.\n ./{filename} {address} {port} {maximum backlog}\n");
+        return 1;
+    }
     if (atoi(argv[3]) < 0) {
         printf("Invalid backlog argument. Must be a positive integer.\n");
         return 1;
@@ -39,7 +49,12 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    struct sockaddr_in saddr = createSockAddr(ADDRESS, PORT, FAMILY);
+    struct sockaddr_in saddr;
+    saddr.sin_family = FAMILY;  // e.g. AF_INET
+    saddr.sin_port = htons(PORT); // Transforms port to unsigned short in network byte order
+    inet_aton(ADDRESS, &saddr.sin_addr);  // Transform ascii ip to an unsigned long in network byte order
+
+
     // Bind the primary socket to {ADDRESS}:{PORT} as described above
     if (bind(sockfd, (struct sockaddr *)(&saddr), sizeof(struct sockaddr_in)) == -1) {
         printf("Error binding the socket.\n");
@@ -51,18 +66,7 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    struct sockaddr_in * connaddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr));
-    socklen_t connsize = sizeof(struct sockaddr_in);
-
-    int connfd = accept(sockfd, (struct sockaddr *)connaddr, &connsize);
-    if (connfd == -1) {
-        printf("Error connecting to socket.\n");
-        return 1;
-    }
-
-    char * message = "Hello world!";
-    send(connfd, message, strlen(message), 0);
+    acceptConnections(sockfd);
     close(sockfd);
-    close(connfd);
     return 0;
 }
